edfb3b8b8ccbead042f087de74c231889aaf926a
maoxikun <106460972+maoxikun@users.noreply.github.com>
2025-10-12 17:23:58 +0800

    Update jetbrains.js

--- jetbrains.js
+++ jetbrains.js
@@ -37,46 +37,50 @@ function getProductCodes() {
 
 function getPluginProductCodes() {
     const productCodes = [];
-    $.getJSON("https://plugins.jetbrains.com/api/searchPlugins", {
-        "max": 10000,
-        "offset": 0,
-        "search": ""
-    }).then((response) => {
-        const products = document.getElementById("products");
-        const tr = document.createElement("tr");
-        const th1 = document.createElement("th");
-        th1.innerText = "Author Name";
-        tr.appendChild(th1);
-        const th2 = document.createElement("th");
-        th2.innerText = "Product Name";
-        tr.appendChild(th2);
-        const th3 = document.createElement("th");
-        th3.innerText = "Product Code";
-        tr.appendChild(th3);
-        products.appendChild(tr);
-        for (let i = 0; i < response.plugins.length; i++) {
-            if (response.plugins[i].pricingModel === "FREE") {
-                continue;
-            }
-            $.getJSON("https://plugins.jetbrains.com/api/plugins/" + response.plugins[i].id).then((response) => {
-                const authorName = response.vendor.publicName !== undefined ? response.vendor.publicName : response.vendor.name;
-                if (authorName === "JetBrains s.r.o.") {
-                    productCodes.push(response.purchaseInfo.productCode);
-                }
+    const products = document.getElementById("products");
+    for (let i = 0; i < 500; i++) {
+        $.getJSON("https://plugins.jetbrains.com/api/searchPlugins", {
+            "max": 20,
+            "offset": 20 * i,
+            "search": ""
+        }).then((response) => {
+            if (i === 0) {
                 const tr = document.createElement("tr");
-                const td1 = document.createElement("td");
-                td1.innerText = authorName;
-                tr.appendChild(td1);
-                const td2 = document.createElement("td");
-                td2.innerText = response.name;
-                tr.appendChild(td2);
-                const td3 = document.createElement("td");
-                td3.innerText = response.purchaseInfo.productCode;
-                tr.appendChild(td3);
+                const th1 = document.createElement("th");
+                th1.innerText = "Author Name";
+                tr.appendChild(th1);
+                const th2 = document.createElement("th");
+                th2.innerText = "Product Name";
+                tr.appendChild(th2);
+                const th3 = document.createElement("th");
+                th3.innerText = "Product Code";
+                tr.appendChild(th3);
                 products.appendChild(tr);
-            });
-        }
-    });
+            }
+            for (let j = 0; j < response.plugins.length; j++) {
+                if (response.plugins[j].pricingModel === "FREE") {
+                    continue;
+                }
+                $.getJSON("https://plugins.jetbrains.com/api/plugins/" + response.plugins[j].id).then((response) => {
+                    const authorName = response.vendor.publicName !== undefined ? response.vendor.publicName : response.vendor.name;
+                    if (authorName === "JetBrains s.r.o.") {
+                        productCodes.push(response.purchaseInfo.productCode);
+                    }
+                    const tr = document.createElement("tr");
+                    const td1 = document.createElement("td");
+                    td1.innerText = authorName;
+                    tr.appendChild(td1);
+                    const td2 = document.createElement("td");
+                    td2.innerText = response.name;
+                    tr.appendChild(td2);
+                    const td3 = document.createElement("td");
+                    td3.innerText = response.purchaseInfo.productCode;
+                    tr.appendChild(td3);
+                    products.appendChild(tr);
+                });
+            }
+        });
+    }
     return productCodes;
 }
 
